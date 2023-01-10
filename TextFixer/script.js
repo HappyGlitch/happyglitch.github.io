89792707c8391a49fea1227c93adfd3f1777334a
Jeremy-Alvin-Jr <88254442+Jeremy-Alvin-Jr@users.noreply.github.com>
2023-01-10 10:12:17 +0100

    Add files via upload

--- TextFixer/script.js
+++ TextFixer/script.js
@@ -1,10 +1,11 @@
 window.onload = function() {
     input = document.getElementById("text");
-    textProblems = { lbf: 0, tabf: 0, spacef: 0, fproblems: 0};
+    textProblems = { lbf: 0, tabf: 0, spacef: 0, fproblems: 0, flyingp: 0, pproblems: 0};
     textProblems.currentTab = "f";
     textProblems.problemButtons = document.getElementsByClassName("problems")[0].children;
     textProblems.problemButtons = [...textProblems.problemButtons].filter(element => element.tagName == "BUTTON");
     textProblems.formattingButton = document.getElementById("formatting");
+    textProblems.punctuationButton = document.getElementById("punctuation");
 }
 
 function onUpdateProblemsTab(value) {
@@ -14,11 +15,12 @@ function onUpdateProblemsTab(value) {
         textProblems.problemButtons[1].innerText = "Tab Formatting(" + textProblems.tabf + ")";
         textProblems.problemButtons[2].innerText = "Space Formatting(" + textProblems.spacef + ")";
     } else if(value == "p") {
-        textProblems.problemButtons[0].innerText = "Flying Comma(0)";
-        textProblems.problemButtons[1].innerText = "Flying Dot(0)";
-        textProblems.problemButtons[2].innerText = "Flying Bracket(0)";
+        textProblems.problemButtons[0].innerText = "Separated Punctuation(" + textProblems.flyingp + ")";
+        textProblems.problemButtons[1].innerText = "-(0)";
+        textProblems.problemButtons[2].innerText = "-(0)";
     }
     textProblems.formattingButton.innerText = "Formatting(" + textProblems.fproblems + ")";
+    textProblems.punctuationButton.innerText = "Punctuation(" + textProblems.pproblems + ")";
 }
 
 function onClickProblemButton(buttonId) {
@@ -38,7 +40,12 @@ function onClickProblemButton(buttonId) {
         }
     } else if(textProblems.currentTab == "p") {
         if(buttonId == 0) {
-
+            while(countFlyingPMarks()) {
+                input.value = input.value.replaceAll(" .", ".");
+                input.value = input.value.replaceAll(" ,", ",");
+                input.value = input.value.replaceAll(" !", "!");
+                input.value = input.value.replaceAll(" ?", "?");
+            }
         } else if(buttonId == 1) {
 
         } else if(buttonId == 2) {
@@ -52,6 +59,10 @@ function countIn(countable, counted) {
     return countable.split(counted).length - 1;
 }
 
+function countFlyingPMarks() {
+    return countIn(input.value, " .") + countIn(input.value, " ,") + countIn(input.value, " !") + countIn(input.value, " ?");
+}
+
 function onTextUpdate() {
     if(input.value < 2)
         textProblems.lbf = 0;
@@ -60,5 +71,8 @@ function onTextUpdate() {
     textProblems.spacef = countIn(input.value, "  ") + countIn(input.value, "\n ");;
     textProblems.tabf = countIn(input.value, "\t");
     textProblems.fproblems = (textProblems.lbf > 0) + (textProblems.spacef > 0) + (textProblems.tabf > 0);
+
+    textProblems.flyingp = countFlyingPMarks();
+    textProblems.pproblems = (textProblems.flyingp > 0) + 0;
     onUpdateProblemsTab(textProblems.currentTab);
 }
